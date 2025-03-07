690557cbebfd973c429d8d2913e1c4612ab21c60
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2025-03-07 00:44:17 -0800

    smooth scroll on more pages

--- js/main.js
+++ js/main.js
@@ -4,32 +4,28 @@ const lenis = new Lenis({
 });
 
 // Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
+lenis.on("scroll", (e) => {
   console.log(e);
 });
 
-
-const positionElement = (e)=> {
+const positionElement = (e) => {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
 
   cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
 
   cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.remove("hidden");
 });
 
-
 function fadeOutAndRedirect(url) {
   document.body.classList.add("hidden");
   setTimeout(() => {
-      window.location.href = url;
+    window.location.href = url;
   }, 800); // Match the transition duration
 }
 
-window.addEventListener('mousemove', positionElement)
-
+window.addEventListener("mousemove", positionElement);
