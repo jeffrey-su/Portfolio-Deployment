9d1a1da7257ad923eaa4b00c613421664cb4c032
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2025-03-06 17:07:14 -0800

    transition fixed + smooth scroll

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const cursorSmall = document.querySelector('.small');
 // const cursorBig = document.querySelector('.big');
 
 
+
 const positionElement = (e)=> {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
@@ -16,11 +17,22 @@ document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.remove("hidden");
 });
 
+
 function fadeOutAndRedirect(url) {
   document.body.classList.add("hidden");
   setTimeout(() => {
       window.location.href = url;
-  }, 400); // Match the transition duration
+  }, 800); // Match the transition duration
 }
 
 window.addEventListener('mousemove', positionElement)
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
