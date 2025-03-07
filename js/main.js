f1ed7557279d51d319848e4bf0229158f77a37e5
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2025-03-06 22:25:47 -0800

    smooth scroll fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-const cursorSmall = document.querySelector('.small');
-// const cursorBig = document.querySelector('.big');
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
 
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
 
 
 const positionElement = (e)=> {
@@ -27,12 +33,3 @@ function fadeOutAndRedirect(url) {
 
 window.addEventListener('mousemove', positionElement)
 
-// Initialize Lenis
-const lenis = new Lenis({
-  autoRaf: true,
-});
-
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
